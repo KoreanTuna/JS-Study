5aa48d5879c831c7b9bd8bc33574dfcee7801349
minwoo <songsee0528@gmail.com>
2025-03-05 22:11:22 +0900

    ✅ export 추가

--- 1_import_and_export/export.js
+++ 1_import_and_export/export.js
@@ -6,3 +6,17 @@ export let url = 'http://www.kakao.com'
 /// 2번 방법 : default
 /// export default는 한 파일에서 한 번만 사용 가능
 export default 'someValue'
+
+/// 함수도 export 가능
+export function test() {
+  console.log('test')
+}
+
+/// 객체도 export 가능
+export const obj = {
+  name: 'object',
+  value: 123,
+}
+
+/// export할 때 이름을 바꿔서 export 가능
+export { apiKey as key }
